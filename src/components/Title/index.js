edf7f87bc4765b6a536c0aa9e08f2fbf1cbba838
Caroline <caroline.portilla@gmail.com>
2020-06-29 14:05:24 +0800

    css

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="title p-5 text-center">{props.children}</h1>;
+  return <h1 className="title p-5 mt-5 text-center">{props.children}</h1>;
 }
 
 export default Title;
