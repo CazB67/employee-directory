2d91ebd13185f97bd138a9106de6790c9d96f60e
Caroline <caroline.portilla@gmail.com>
2020-06-29 21:05:46 +0800

    responsive title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="title p-5 mt-5 text-center">{props.children}</h1>;
+  return <nav className="navbar">
+  <span className="navbar-brand mx-auto mb-0 p-3 h1">{props.children}</span>
+</nav>;
 }
 
 export default Title;
