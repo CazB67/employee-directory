939a8398b914d681167b4d5cb55984f82252c09e
Caroline <caroline.portilla@gmail.com>
2020-07-01 12:05:27 +0800

    filtered first name

--- src/App.js
+++ src/App.js
@@ -10,19 +10,38 @@ import employees from "./employees.json";
 class App extends Component {
   // Setting this.state.employees to the employees json array
   state = {
-   employees
+   employeeInfo: [],
+   filter: ""
+  };
+
+  componentDidMount() {
+    this.setState({ employeeInfo: employees });
+  }
+
+  filterFirstName = (event) => {
+    this.setState({filter: event.target.value})
+  }
+
+  filterData = () => {
+    let employeeFilter = this.state.employeeInfo;
+    if(this.state.filter === "") {
+      return employeeFilter
+    }
+    return employeeFilter.filter(employee => employee.firstName.toLowerCase().includes(this.state.filter.toLowerCase()) )
   };
 
   render() {
     return (
       <Wrapper>
         <Container>
-        <Title></Title>
-        <SearchBox></SearchBox>
+        <Title/>
+        <SearchBox 
+          filterEvent = {this.filterFirstName}
+        />
         <div className="table-responsive">
         <table className="table table-hover table-dark table-responsive-sm">
-          <TableHeader></TableHeader>
-            {this.state.employees.map(employee => (
+          <TableHeader/>
+            {this.filterData().map(employee => (
               <EmployeeTable
                 id={employee.id}
                 key={employee.id}
--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
 
-function SearchBox() {
-  return <div><input className="mb-3 mt-3 text-center" placeholder="Search"></input>
+  function SearchBox(props) {
+  return <div><input className="mb-3 mt-3 text-center" onChange={props.filterEvent} placeholder="Search"></input>
             </div>
 }
 
